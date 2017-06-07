d9ef9743e8186c8a1e31ed53f8a84db27b9d0474
MrEyelet <marcin.adam.mroczko@gmail.com>
2017-06-07 21:02:41 +0200

    changes

--- js/script_sticky_navigation.js
+++ js/script_sticky_navigation.js
@@ -1,22 +1,5 @@
 $(function() {
-	/*var NavY = $('nav').offset().top;
-	 
-	function stickyNav(){
-		var ScrollY = $(window).scrollTop();
-		  
-		if (ScrollY > NavY) { 
-			$('nav').addClass('sticky');
-		} else {
-			$('nav').removeClass('sticky'); 
-		}
-	};
-	 
-	stickyNav();
-	 
-	$(window).scroll(function() {
-		stickyNav();
-	});
-*/
+
 	$(window).scroll(function() {
 		var navHeight = $(window).height();//this measure height of the whole window
     	if  ($(window).scrollTop() > navHeight ) {//scrollTop is vertical scrollbar position from the top
@@ -25,6 +8,5 @@ $(function() {
       		$('nav').removeClass('sticky');
     	}
   	});
-  //alert($(window).height());
-  //alert($(window).scrollTop());
+
 });
